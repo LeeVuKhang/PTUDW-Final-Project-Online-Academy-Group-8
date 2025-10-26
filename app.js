25726813449a3ed8810b43b03f79357a0ee66fbf
LeeVuKhang <lvukhangpq@gmail.com>
2025-10-27 01:58:59 +0700

    debug, add button to navbar and user dropbox

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import Handlebars from 'handlebars';
 
 import categoryModel from './models/category.model.js';
 import * as courseModel from './models/course.model.js';
-import { checkAdmin, checkAuthenticated } from './models/auth.model.js';
+import { checkAdmin, checkAuthenticated, checkInstructor} from './models/auth.model.js';
 import ratingModel from './models/rating.model.js';
 
 
--- models/course.model.js
+++ models/course.model.js
@@ -59,4 +59,47 @@ export async function findImpressiveCoursesLastWeek(limit = 4) {
     .orderBy('c.views', 'desc')                   // sắp xếp theo lượt xem giảm dần
     .limit(limit);
 }
+export async function findCoursesByFilter(categoryId, studentId, limit, offset) {
+  let query = db("courses as c")
+    .join("categories as cat", "c.catid", "cat.cat_id")
+    .leftJoin("users as u", "c.instructor_id", "u.user_id")
+    .leftJoin("ratings as r", "c.course_id", "r.course_id")
+    .select(
+      "c.course_id", "c.title", "c.price", "c.discount_price", "c.image_url",
+      "cat.cat_name",
+      "u.name as instructor_name", "u.user_id as instructor_id",
+      db.raw("COALESCE(AVG(r.value), 0) as avg_rating"),
+      db.raw("COUNT(DISTINCT r.rating_id) as rating_count")
+    )
+    .groupBy("c.course_id", "cat.cat_name", "u.name", "u.user_id");
 
+  if (categoryId && categoryId !== 0 && categoryId !== 'all') {
+    query.where("c.catid", categoryId);
+  }
+
+  if (studentId) {
+    query.select(
+      db.raw(
+        `EXISTS (
+          SELECT 1 FROM watchlists w 
+          WHERE w.course_id = c.course_id AND w.student_id = ?
+        ) as "isInWatchlist"`,
+        [studentId]
+      )
+    );
+  } else {
+    query.select(db.raw('false as "isInWatchlist"'));
+  }
+
+  return query.limit(limit).offset(offset);
+}
+
+export async function countCoursesByFilter(categoryId) {
+  let countQuery = db("courses");
+
+  if (categoryId && categoryId !== 0 && categoryId !== 'all') {
+    countQuery.where("catid", categoryId);
+  }
+  
+  return countQuery.count("* as amount").first();
+}
